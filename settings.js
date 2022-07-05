b614bc280b7e7121060dc26c5eac740e702882e2
Anonymousbot8900 <107625853+Anonymousbot8900@users.noreply.github.com>
2022-07-05 13:21:48 +0700

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,105 @@
+/*
+› Create By @RIY
+› Base Ori @DikaArdnt
+› Kalau mau tambahin fitur di index.js
+› Edit text menu di file language › help.js
+*/
+
+//gausah di apa² in!
+const fs = require('fs')
+const chalk = require('chalk')
+
+//—————「 Website Apikey 」—————//
+global.APIs = {
+//gausah di apa² in!
+zenz: 'https://zenzapis.xyz',
+}
+
+//—————「 Website Apikey 」—————//
+global.APIKeys = {
+//gausah di apa² in, udah free apikey :)
+'https://zenzapis.xyz': 'BagasGanz',
+}
+
+//—————「 Setting Owner 」—————//
+global.owner = ['6281232322106','628818837894'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
+global.premium = ['62815232322106'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
+global.ownernomer = '6281232322106' //ubah jadi nomor mu, note tanda ' gausah di hapus!
+
+//—————「 Set Nama Own & Bot 」—————//
+global.ownername = 'Ryan' //ubah jadi nama mu, note tanda ' gausah di hapus!
+global.botname = 'RyBot-MD' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
+global.footer = 'RyBot' //ubah jadi nama mu, note tanda ' gausah di hapus!
+
+//—————「 Set Kebutuhan Button & Kontak 」—————//
+//ubah aja kalau ada yang gapunya gsh di hps anggap aja credits :), note tanda ' gausah di hapus!
+global.youtube = ''
+global.ig = 'https://instagram.com/jetprintstore'
+global.mygc = ''
+global.myweb = 'https://jetprintstore.blogspot.com'
+global.email = 'jetprintstore@gmail.com'
+global.region = 'indonesia'
+
+//—————「 Set Wm 」—————//
+global.packname = '© RyBot-MD' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
+global.author = 'Ryan' //ubah jadi nama mu, note tanda ' gausah di hapus!
+
+//—————「 Set Nama Session 」—————//
+//gausah di apa² in!
+global.sessionName = 'session'
+
+//—————「 Set Limit 」—————//
+//terserah mau ubah atau nggak, note : tanda ' gausah di hapus!
+global.limitawal = {
+premium: "Infinity",
+free: 15,
+}
+
+//—————「 Set Image 」—————//
+//terserah mau ubah apa nggak, menurut gua jangan di ubah!
+global.thumb = fs.readFileSync('./image/chika.jpg')
+/*global.veloriy = fs.readFileSync('./image/chika.mp4')*/
+
+//—————「 Set Image Welcome 」—————//
+//terserah mau ubah apa nggak, menurut gua jangan di ubah!
+global.BgWelcLeav = 'https://scontent.fsub3-1.fna.fbcdn.net/v/t1.6435-9/67405411_446980379487388_7377263173113479168_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=6fIraN4IUZAAX93dwOR&_nc_ht=scontent.fsub3-1.fna&oh=00_AT-jwIz2_d3RLvKinm_kuUUlAb7AFr4ddc-n89kYMvLp_w&oe=62E65A62'
+
+//—————「 Set Random Image Menu 」—————//
+//gausah di apa² in!
+global.flaming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.fluming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=fluffy-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.flarun = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=runner-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.flasmurf = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=smurfs-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+
+//—————「 Set Prefix 」—————//
+//gausah di apa² in!
+global.prefa = ['','!','.','🐦','🐤','🗿']
+
+//—————「 Set Simbol 」—————//
+//terserah mai ubah atau nggak, note : tanda ' gausah di hapus!
+global.sp = '⭔'
+
+//—————「 Set Message 」—————//
+//terserah mau ubah apa nggak, note : tanda ' gausah di hapus!
+global.mess = {
+success: '_Done._',
+admin: '_Fitur Khusus Admin Group!_',
+botAdmin: '_Bot Harus Menjadi Admin Terlebih Dahulu!_',
+owner: '_Fitur Khusus Owner Bot_',
+group: '_Fitur Digunakan Hanya Untuk Group!_',
+private: '_Fitur Digunakan Hanya Untuk Private Chat!_',
+bot: '_Fitur Khusus Pengguna Nomor Bot_',
+wait: '_Sedang Di Proses..._',
+error: '_Fitur Sedang Error!_',
+endLimit: '_Limit Harian Anda Telah Habis, Limit Akan Direset Setiap Jam 12_',
+}
+
+//—————「 Batas Akhir 」—————//
+//gausah si apa² in!
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+fs.unwatchFile(file)
+console.log(chalk.redBright(`Update'${__filename}'`))
+delete require.cache[file]
+require(file)
+})
